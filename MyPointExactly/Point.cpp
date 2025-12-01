876be914a6348a6052ef1b8db4e6ca0353a7af50
Yahli Waizman <yahli.waizman@gmail.com>
2025-12-01 18:30:02 +0200

    finished part 1

--- MyPointExactly/Point.cpp
+++ MyPointExactly/Point.cpp
@@ -3,15 +3,15 @@
 
 Point::Point()
 {
-	_coord = new int[2];
-	_coord[0] = _coord[1] = 0;
+    _coord = new int[2];
+    _coord[0] = _coord[1] = 0;
 }
 
 Point::Point(int x, int y)
 {
-	_coord = new int[2];
-	_coord[0] = x;
-	_coord[1] = y;
+    _coord = new int[2];
+    _coord[0] = x;
+    _coord[1] = y;
 }
 
 Point::Point(const Point& other)
@@ -28,7 +28,7 @@ Point::~Point()
 
 Point& Point::operator=(const Point& other)
 {
-    if (this != &other) 
+    if (this != &other)
     {
         delete[] _coord;
         _coord = new int[2];
@@ -38,19 +38,19 @@ Point& Point::operator=(const Point& other)
     return *this;
 }
 
-int Point::getX() const 
-{ 
-	return _coord[0]; 
+int Point::getX() const
+{
+    return _coord[0];
 }
 
 int Point::getY() const
-{ 
-	return _coord[1]; 
+{
+    return _coord[1];
 }
 
 
 std::ostream& operator<<(std::ostream& os, const Point& p)
 {
-	os << "(" << p._coord[0] << "," << p._coord[1] << ")";
-	return os;
+    os << "(" << p._coord[0] << "," << p._coord[1] << ")";
+    return os;
 }
--- MyPointExactly/main.cpp
+++ MyPointExactly/main.cpp
@@ -7,8 +7,8 @@ int main()
 	Point p1(1, 2);
 	Point p2 = p1;
 
-	std::cout << "p1="<< p1 << std::endl;
-	std::cout << "p2=" << p2<< std::endl;
+	std::cout << "p1=" << p1 << std::endl;
+	std::cout << "p2=" << p2 << std::endl;
 
 	return 0;
 }
